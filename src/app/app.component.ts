9c31d7c4c356247eca02873ad2b3aa75b6ddee56
Bang <KSB0711@daugherty.com>
2017-10-31 09:43:33 -0400

    Changing app component to have todo features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
+
 @Component({
   selector: 'td-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'td';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
